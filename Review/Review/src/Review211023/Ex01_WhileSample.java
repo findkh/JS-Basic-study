8784c78a2ffb34aa452e0e63767df89735d2fb59
박경현 <findkh@nate.com>
2021-10-23 14:09:20 +0900

    211023 복습

---
+++ Review/Review/src/Review211023/Ex01_WhileSample.java
@@ -0,0 +1,22 @@
+package Review211023;
+
+import java.util.Scanner;
+
+public class Ex01_WhileSample {
+	public static void main(String[] args) {
+//		while문을 이용하여 키보드에서 숫자를 입력 받아 입력 받은 모든 수의 평균을 출력하는 프로그램
+//		0이 입력되면 입력이 종료되고 평균을 구하여 출력한다
+		
+		Scanner rd = new Scanner(System.in);
+		int n = 0;
+		float sum = 0;
+		int i = 0;
+		while((i=rd.nextInt()) != 0) {
+			sum += i;
+			n++;
+		}
+		System.out.println("입력된 수의 개수는" + n + "개" + "평균은 " + sum/n + "입니다");
+
+	}
+
+}
