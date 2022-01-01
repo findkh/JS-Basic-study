e892d036bc46520dcdea86f210f13d86edb2c8e3
KyoungHyunPark <findkh@nate.com>
2022-01-01 19:10:36 +0900

    220101

--- Doit-JS-basic/07/js/quiz-1.js
+++ Doit-JS-basic/07/js/quiz-1.js
@@ -1,5 +1,26 @@
 		var numbers = [2, 4, 6, 8, 10];
+
 		showArray(numbers);
+
+		// var sum = 0;
+		// for(let i=0; i<numbers.length; i++) {
+		// 	sum += numbers[i];
+		// }
+		// numbers.push(sum);
+		// showArray(numbers);
+
+		var total = [];
+		totalSum = 0;
+		numbers.forEach((num)=>{
+			totalSum += num;
+		});
+		// console.log(totalSum);
+		total.push(totalSum);
+		showArray(total)
+
+		;
+		showArray(numbers.concat(total))
+		
 
 		function showArray(arr) {
 			var str = "<table><tr>";
