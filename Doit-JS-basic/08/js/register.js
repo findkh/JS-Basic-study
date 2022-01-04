5668bde95ba0262682cdf854a36fb4a4a272823f
KyoungHyunPark <findkh@nate.com>
2022-01-05 00:31:02 +0900

    220105

---
+++ Doit-JS-basic/08/js/register.js
@@ -0,0 +1,26 @@
+function newRegister() {
+    var newP = document.createElement("p");
+    var userName = document.querySelector("#userName");
+    var newText = document.createTextNode(userName.value);
+    newP.appendChild(newText);
+
+    var delBtn = document.createElement("span");
+    var delText = document.createTextNode("X");
+    delBtn.setAttribute("class", "del");
+    delBtn.appendChild(delText);
+    newP.appendChild(delBtn);
+
+
+    var nameList = document.querySelector("#nameList");
+    nameList.insertBefore(newP, nameList.childNodes[0]);
+    userName.value = "";
+
+    var removeBtn = document.querySelectorAll(".del");
+
+    for (var i = 0; i < removeBtn.length; i++) {
+        removeBtn[i].addEventListener("click", function(){
+            if (this.parentNode.parentNode)
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        });
+    }
+}
