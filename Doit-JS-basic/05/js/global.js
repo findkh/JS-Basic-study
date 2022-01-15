bfc650a2a5912f44a4b441ed8920e482a1915c08
KyoungHyunPark <89373222+findkh@users.noreply.github.com>
2022-01-15 19:55:49 +0900

    .
    .

--- Doit-JS-basic/05/js/global.js
+++
@@ -1,7 +0,0 @@
-var myVar = 100; //전역 변수 선언
-test();
-document.write("myVar is " + myVar);
-
-function test() { 			
-	myVar = 50; //전역 변수가 된다
-}
--- Doit-JS-basic/05/js/hoisting.js
+++
@@ -1,8 +0,0 @@
-var x = 100;
-
-test();
-
-function test() { 			
-  var y = 200;
-  document.write("x is " + x + ", y is " + y);
-}
